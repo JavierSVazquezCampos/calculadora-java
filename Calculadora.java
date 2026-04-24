3bdff3e80fc45123893f51a8678f2b35c577007a
JavierSVazquezCampos <javierserafin2002@gmail.com>
2026-04-24 21:00:37 +0200

    Añadida funcionalidad de suma

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,9 @@
 public class Calculadora {
     public static void main(String[] args){
-        System.out.println(" Calculadora Iniciada ")
+        System.out.println(" Calculadora Iniciada ");
+
+        Suma sum = new Suma();
+        
+        System.out.println("Suma: " + sum.sumar(10, 5));
     }
 }
