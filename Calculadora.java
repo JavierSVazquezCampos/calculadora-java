cced33846090d9ed7dbb73bb2cd11bf0f67acfc3
JavierSVazquezCampos <javierserafin2002@gmail.com>
2026-04-29 20:47:42 +0200

    Programador B: Anadida funcionalidad de resta

--- Calculadora.java
+++ Calculadora.java
@@ -1,5 +1,12 @@
 public class Calculadora {
     public static void main(String[] args){
         System.out.println(" Calculadora Iniciada ")
+        
+        Resta rest = new Resta();
+
+
+        System.out.println("Resta: " + rest.restar(10, 5));
+    }
+
     }
 }
