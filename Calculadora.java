deffad33b705888d1bedf03ee61e986fe483bcb7
JavierSVazquezCampos <javierserafin2002@gmail.com>
2026-04-29 22:11:36 +0200

    Añadida division

--- Calculadora.java
+++ Calculadora.java
@@ -2,6 +2,10 @@ public class Calculadora {
     public static void main(String[] args){
         System.out.println(" Calculadora Iniciada ")
         
+        Resta rest = new Resta();
+        
+        System.out.println("Resta: " + rest.restar(10, 5));
+
         Division div = new Division();
 
         System.out.println("División: " + div.dividir(10, 5));
